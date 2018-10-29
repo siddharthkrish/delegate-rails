bf6788fcb44322f0684fdcf68fc4b910f0219556
Siddharth Krish <siddharth.krish@moneysmart.sg>
2018-10-29 08:33:51 +0800

    post data working without CSRF

--- login/app/controllers/login_controller.rb
+++ login/app/controllers/login_controller.rb
@@ -1,5 +1,24 @@
 class LoginController < ApplicationController
+  # skip CSRF protection and set session to null
+  # protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
+
+  # this is just for demo & talking about how the functionality
+  # can be achieved. in production, don't blindly redirect to the
+  # callback, make sure you've implemented a whitelist & have
+  # people register the callback to avoid redirect attacks
+
   def index
+    @callback = session[:callback]
+    @payload = session[:payload]
+  end
+
+  def data
+    @payload = params[:payload]
     @callback = params[:callback]
+
+    session[:payload] = @payload
+    session[:callback] = @callback
+    redirect_to action: :index
   end
 end
--- login/config/routes.rb
+++ login/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'login/index'
+  post 'login/data'
   root 'login#index'
 end
