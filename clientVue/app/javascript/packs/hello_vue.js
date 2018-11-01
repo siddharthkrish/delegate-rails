1e756f7da1326c2e3bd43caa7a16f6a0b59d005e
Sid <siddharth.krish@moneysmart.co>
2018-11-01 18:47:10 +0800

    added vue client

---
+++ clientVue/app/javascript/packs/hello_vue.js
@@ -0,0 +1,10 @@
+import Vue from 'vue/dist/vue.esm'
+
+document.addEventListener('DOMContentLoaded', () => {
+  var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!'
+    }
+  })
+})
---
+++ clientVue/config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get 'pages/welcome'
+  root 'pages#welcome'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
